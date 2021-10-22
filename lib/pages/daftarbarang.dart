35a31e4d22509f97c0883ce1c85776cda05c03d7
fuadagus <fagussalim518@gmail.com>
2021-10-23 03:08:50 +0700

    styling pada widget list tile

--- lib/pages/daftarbarang.dart
+++ lib/pages/daftarbarang.dart
@@ -9,8 +9,8 @@ class DaftarBarang extends StatefulWidget {
 }
 
 class _DaftarBarangState extends State<DaftarBarang> {
-  final items = List.generate(100, (index) => "item $index");
-  final jumlahStok = List.generate(100, (index) => "$index");
+  final items = List.generate(1000, (index) => "item $index");
+  final jumlahStok = List.generate(1000, (index) => "$index");
   @override
   Widget build(BuildContext context) {
     print(MediaQuery.of(context).size);
@@ -42,77 +42,90 @@ class _DaftarBarangState extends State<DaftarBarang> {
             child: Container(
               height: MediaQuery.of(context).size.height,
               child: ListView.builder(
-                  itemExtent: 60,
+                  itemExtent: 80,
                   itemCount: items.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: Container(
-                        color: Colors.white,
-                        height: 59,
-                        child: Column(
-                          children: [
-                            Expanded(
-                              flex: 1,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(left: 2, right: 2),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Expanded(
-                                      flex: 6,
-                                      child: Padding(
-                                        padding: const EdgeInsets.only(
-                                            left: 4, right: 4),
-                                        child: Text(
-                                          items[index],
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
+                      title: Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.amber,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3))
+                              ]),
+                          // color: Colors.white,
+                          height: 59,
+                          child: Column(
+                            children: [
+                              Expanded(
+                                flex: 1,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.only(left: 2, right: 2),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Expanded(
+                                        flex: 6,
+                                        child: Padding(
+                                          padding: const EdgeInsets.only(
+                                              left: 4, right: 4),
+                                          child: Text(
+                                            items[index],
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    if (MediaQuery.of(context).size.width >
-                                        1041)
-                                      Expanded(
-                                        flex: 1,
-                                        child: Text(
-                                          "Stok: " + jumlahStok[index],
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
+                                      if (MediaQuery.of(context).size.width >
+                                          1041)
+                                        Expanded(
+                                          flex: 1,
+                                          child: Text(
+                                            "Stok: " + jumlahStok[index],
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
                                         ),
-                                      ),
-                                    if (MediaQuery.of(context).size.width < 400)
-                                      Expanded(
-                                          child: Row(
-                                        children: [
-                                          Expanded(
-                                              flex: 5,
-                                              child: Text(jumlahStok[index])),
-                                          Expanded(
-                                            flex: 5,
-                                            child: IconButton(
-                                                onPressed: () {},
-                                                icon: Icon(Icons.edit)),
-                                          )
-                                        ],
-                                      )),
-                                    if (MediaQuery.of(context).size.width >
-                                        1041)
-                                      Expanded(
-                                        flex: 1,
-                                        child: IconButton(
-                                          icon: Icon(Icons.edit),
-                                          onPressed: () {},
+                                      if (MediaQuery.of(context).size.width <
+                                          400)
+                                        Expanded(
+                                            flex: 3,
+                                            child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.end,
+                                              children: [
+                                                Text(jumlahStok[index]),
+                                                IconButton(
+                                                    onPressed: () {},
+                                                    icon: Icon(Icons.edit))
+                                              ],
+                                            )),
+                                      if (MediaQuery.of(context).size.width >
+                                          1041)
+                                        Expanded(
+                                          flex: 1,
+                                          child: IconButton(
+                                            icon: Icon(Icons.edit),
+                                            onPressed: () {},
+                                          ),
                                         ),
-                                      ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                            Expanded(
-                                flex: 1, child: Text("diaupdate oleh:...")),
-                          ],
+                              Expanded(
+                                  flex: 1, child: Text("diupdate oleh:...")),
+                            ],
+                          ),
                         ),
                       ),
                     );
